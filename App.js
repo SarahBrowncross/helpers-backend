453d9a892a9624ce303384e4bbd1b8ebfa60d0f4
SarahBrowncross <sarah.browncross@gmail.com>
2020-11-05 11:23:14 +0000

    implememt express server and json storage

---
+++ App.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const fs = require('fs').promises;
+
+const { PORT = 8080 } = process.env;
+
+const app = express();
+const cors = require('cors')
+
+
+app.use(cors())
+app.use(express.json())
+
+app.options('*', cors())
+
+app.get('/playerlist', (req, res) => {
+	fs.readFile('./players.json', 'utf-8')
+	.then((file) =>{
+		const parsedFile = JSON.parse(file)
+		res.json(parsedFile)
+	})
+})
+
+app.post('/playerlist', (req, res) => {
+	const name = req.body.name;
+	fs.readFile('./scores.json', 'utf-8')
+	.then((file) => {
+		const parsedFile = JSON.parse(file)
+		parsedFile.unshift({name})
+		fs.writeFile('./scores.json', JSON.stringify(parsedFile))
+		.then(() => {
+			res.json(parsedFile)
+		})
+	})
+})
+
+app.listen(PORT, () => {
+	console.log('listening');
+})
